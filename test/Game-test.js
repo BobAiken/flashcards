b3d6c783d99e4b574823b32e96e123c429c5af07
Robert Aiken <robertsiraaiken@gmail.com>
2022-11-30 18:51:15 -0600

    Rename test

--- test/Game-test.js
+++ test/Game-test.js
@@ -14,7 +14,7 @@ describe("Game", () => {
 
   it("should be a function", () => expect(Game).to.be.a("function"));
 
-  it("should be an instance of Turn", () =>
+  it("should be an instance of Game", () =>
     expect(game).to.be.an.instanceof(Game));
 
   it("should keep track of the current round", () =>
